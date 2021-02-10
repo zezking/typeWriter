0c63073ccf4d5bae7831c2849c41500a5c5b2ead
zezking <zhaoenze001@gmail.com>
2021-02-10 18:32:36 +0000

    refactor timer without recursion

--- typeWriter.js
+++ typeWriter.js
@@ -1,12 +1,22 @@
 const typeWriter = (str) => {
-  if (str.length > 1) {
+  // if (str.length > 1) {
+  //   setTimeout(() => {
+  //     process.stdout.write(str[0]);
+  //     str = str.substring(1);
+  //     typeWriter(str);
+  //   }, 50);
+  // } else {
+  //   process.stdout.write(str[0] + "\n");
+  // }
+  let timer = 0;
+  for (let i = 0; i < str.length; i++) {
     setTimeout(() => {
-      process.stdout.write(str[0]);
-      str = str.substring(1);
-      typeWriter(str);
-    }, 50);
-  } else {
-    process.stdout.write(str[0] + "\n");
+      process.stdout.write(str[i]);
+      if (i === str.length - 1) {
+        process.stdout.write(`\n`);
+      }
+    }, (timer += 50));
   }
 };
-typeWriter("hello there from lighthouasdfasdfasdfse labs!");
+
+typeWriter("hello there from labs!");
